0b7bc2baf7971119d62431aa0abac49782aa0f6c
Bill Watters <billywatters13@gmail.com>
2020-12-14 13:24:40 -0500

    Add jwt for login

--- back-end/src/routes/login.js
+++ back-end/src/routes/login.js
@@ -1,29 +1,39 @@
-const bcrypt = require('bcrypt')
+const bcrypt = require('bcrypt');
+const jwtGenerator = require('../utils/jwtGenerator');
 const router = require("express").Router();
 module.exports = db => {
   router.post("/login", async (req, res) => {
-    const { email, password } = req.body
-    db.query(`
-      SELECT *
-      FROM users 
-      WHERE email= $1 
-      `
-      , [email])
-      .then(async ({ rows }) => {
-        if (rows.length !== 0) {
-          if (await bcrypt.compare(password, rows[0].password)) {
-            req.session.user_id = rows[0].id
-            res.send('Success')
-          } else {
-            res.send('Invalid email or password')
-          }
-        } else {
-          return res.status(400).send('There is not a user with the provided email')
-        }
-      })
-      .catch((err) => {
-        res.status(500).send(err)
-      })
+    try {
+      //Destructure the body
+      const { email, password } = req.body
+      //Check id the user exists
+      const user = await db.query(`
+        SELECT *
+        FROM users 
+        WHERE email= $1 `
+        , [email]
+      )
+      // If the user doesnt exist
+      if (user.rows.length === 0) {
+        return res.status(401).send('There is not a user with the provided email')
+      }
+
+      //Check if the password is correct 
+      const validPassword = await bcrypt.compare(password, user.rows[0].password)
+
+      if (!validPassword) {
+        return res.status(401).send('Invalid email or password')
+      }
+
+      //Create jwt
+      const token = jwtGenerator(user.rows[0].id)
+
+      res.json({ token })
+
+    } catch (err) {
+      console.error(err.message)
+      res.status(500).send("Server Error")
+    }
   })
   return router;
 }
